cc84001cfd85a8aae358d49c031c8ba286b1a728
Steliyan03 <rumenovsteliyan0@gmail.com>
2024-11-24 01:17:29 +0200

    Add files via upload

---
+++ Раздел 7 задача/Раздел 7 задача/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int height = int.Parse(Console.ReadLine());
+        string side = Console.ReadLine().ToLower();
+
+        if (side == "right")
+        {
+            
+            for (int i = 1; i <= height; i++)
+            {
+                Console.Write(new string(' ', height - i));
+                Console.Write("*");
+
+                if (i > 1)
+                {
+                    Console.Write(new string(' ', i - 2));
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+        else if (side == "left")
+        {
+
+            for (int i = 1; i <= height; i++)
+            {
+                Console.Write("*");
+
+
+                if (i > 1)
+                {
+                    Console.Write(new string(' ', i - 2));
+                    Console.Write("*");
+                }
+
+
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid orientation. Please enter 'left' or 'right'.");
+        }
+    }
+}
+
